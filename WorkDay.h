01ba35ab2190c1ea6ca1fcf0be49edb434ca5057
Randy Hash <hashr25@mycu.concord.edu>
2014-09-24 11:53:32 -0400

    Added WorkDay Class and Test Main
    Added WorkDay Class files and added to project and added test main to
    subfolder

---
+++ WorkDay.h
@@ -0,0 +1,47 @@
+#ifndef WORKDAY_H
+#define WORKDAY_H
+
+#include "MyDate.h"
+#include "MyTime.h"
+
+class WorkDay
+{
+public:
+    ///Constructors and Destructors
+    WorkDay();
+    WorkDay( MyTime, MyTime );
+    WorkDay( MyTime, MyTime, MyTime, MyTime );
+    ~WorkDay();
+
+    ///Getters and Setters
+    MyDate getDate();
+    MyTime getScheduledStartTime();
+    MyTime getScheduledEndTime();
+    MyTime getClockInTime();
+    MyTime getClockOutTime();
+
+    void setDate( MyDate );
+    void setScheduledStartTime( int, int, int );
+    void setScheduledEndTime( int, int, int );
+    void setClockInTime( MyTime );
+    void setClockOutTime( MyTime );
+
+    ///Member Methods
+    void clockIn();
+    void clockOut();
+    float calculateHoursScheduled();
+    float calculateHoursWorked();
+
+private:
+    MyDate date;
+
+    MyTime scheduleStartTime;
+    MyTime scheduleEndTime;
+    MyTime clockInTime;
+    MyTime clockOutTime;
+
+    float hoursScheduled;
+    float hoursWorked;
+};
+
+#endif // WORKDAY_H
